2324aa19d58e379e8f0f81410e00c808724ac1d7
GeneralNZR <zeitoun.noam@gmail.com>
2021-09-23 11:40:23 +0200

    updates test

--- back/app/router.js
+++ back/app/router.js
@@ -78,20 +78,94 @@ router.get(`/wiki/:title(/^[^\s.]{1,255}$/gm)`, cController.findByPk);
 
 //#region POST
 
-// POST /profile
+// POST /signup
 
 /**
  * Add a new user in database
- * @route POST /profile
+ * @route POST /signup
  * @group User
  * @summary Add a new user in database
- * @param {ReqProfileJson.model | User.model} object.body.required - Object containing the properties to insert/update an user
  * @returns {User.model} 201 - The newly created user
  * @returns {User.model} 204 - The updated profile
  * @returns {string} 500 - An error message
  * @returns {string} 400 - A validation error message
  */
-router.post('/profile', aController.save);
+router.post('/signup', aController.save);
+
+// POST /signin
+
+/**
+ * Retrieve connection informations from a user
+ * @route POST /signin
+ * @group User
+ * @summary Retrieve connection informations from a user
+ * @returns {User.model} 200 - The user
+ * @returns {string} 500 - An error message
+ * @returns {string} 400 - A validation error message
+ */
+router.post('/signin', aController.findByEmail);
+
+// POST /campaigns
+
+/**
+ * Add a new campaign in database
+ * @route POST /campaigns
+ * @group Campaigns
+ * @summary Add a new campaign in database
+ * @returns {Campaign.model} 201 - The newly created campaign
+ * @returns {string} 500 - An error message
+ * @returns {string} 400 - A validation error message
+ * @returns {string} 409 - A conflict error message
+ * @returns {string} 401 - An unauthorized error message
+ */
+router.post('/campaigns', bController.save);
+
+// POST /campaigns/:campaign_name
+
+/**
+ * Add a note and/or a new player to a campaign
+ * @route POST /campaigns/{campaign_name}
+ * @group Campaigns
+ * @summary Add a note and/or a new player to a campaign
+ * @param {string} campaign_name - The name of the campaign to update
+ * @returns {Campaign.model} 201 - The newly created note / added player
+ * @returns {string} 500 - An error message
+ * @returns {string} 400 - A validation error message
+ * @returns {string} 409 - A conflict error message
+ * @returns {string} 401 - An unauthorized error message
+ */
+router.post(`/campaigns/:campaign_name(/^[^\s.]{1,255}$/gm)`, cController.update);
+
+// POST /wiki
+
+/**
+ * Add a new wiki in database
+ * @route POST /wiki
+ * @group Wiki
+ * @summary Add a new wiki in database
+ * @returns {Wiki.model} 201 - The newly created wiki
+ * @returns {string} 500 - An error message
+ * @returns {string} 400 - A validation error message
+ * @returns {string} 409 - A conflict error message 
+ */
+router.post('/wiki', dController.save);
+
+// POST /wiki/:title
+
+/**
+ * Add a new wiki to a region in database
+ * @route POST /wiki/{title}
+ * @group Wiki
+ * @summary Add a new wiki to a region in database
+ * @param {string} title - The title of the wiki to update
+ * @returns {Wiki.model} 201 - The newly created wiki
+ * @returns {string} 500 - An error message
+ * @returns {string} 400 - A validation error message
+ * @returns {string} 409 - A conflict error message
+ * @returns {string} 401 - An unauthorized error message
+ * @returns {string} 404 - A not found error message
+ */
+router.post(`/wiki/:title(/^[^\s.]{1,255}$/gm)`, dController.update);
 
 //#endregion POST
 
