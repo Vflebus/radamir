e813cd79b4858f382eb77a45c239b910647eb845
Michael-M <mt.morales@hotmail.fr>
2021-09-23 13:58:45 +0200

    added nav arrow in wiki index

--- front/src/components/Wikis/index.js
+++ front/src/components/Wikis/index.js
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
+
 import WikiSection from "./WikiSection";
 
 import "./wikis.scss";
+
+import arrow from "../../assets/images/flecheNavRouge.png";
 
 const regions = [
   { name: "Vanna", route: "vanna" },
@@ -15,6 +19,10 @@ const others = [{ name: "Magie", route: "magie" }];
 const Wikis = () => {
   return (
     <div className="wikis">
+      <Link to="/" className="to-home">
+        Accueil
+        <img src={arrow} alt="Retour à l'accueil" />
+      </Link>
       <h1 className="wikis__title">Index du Wiki</h1>
       <WikiSection title="Régions" links={regions} />
       <WikiSection title="Informations Générales" links={others} />
