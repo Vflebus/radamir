0b5d9afccb3e822f157d8cffe2f362758e918185
Loïc Bonaventura <loic.bonaventura@gmail.com>
2021-09-24 11:21:39 +0200

    Deleted table wiki_has_block in initdb sqitch migration

--- back/migrations/verify/initdb.sql
+++ back/migrations/verify/initdb.sql
@@ -8,7 +8,6 @@ SELECT id FROM campaign_has_players WHERE false;
 SELECT id FROM note WHERE false;
 SELECT id FROM wiki WHERE false;
 SELECT id FROM "block" WHERE false;
-SELECT id FROM wiki_has_blocks WHERE false;
 SELECT id FROM region WHERE false;
 
 ROLLBACK;
