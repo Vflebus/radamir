ef75df9cbb118323083335442c84776976637060
Vincent Flebus <vflebus@gmail.com>
2021-09-27 11:52:45 +0200

    Scroll indicator done, still trying to find a suitable style (no success so far)

--- front/src/components/Wiki/WikiCategory.js
+++ front/src/components/Wiki/WikiCategory.js
@@ -2,10 +2,7 @@ import wikiParchment from "../../assets/images/wikiParchment.png"
 
 const WikiCategory = ({ title, content }) => {
   return (
-    <div
-      className="wiki__category"
-      id={`wiki__category-${title.toLowerCase()}`}
-    >
+    <div className="wiki__category" id={`wiki__category-${title.toLowerCase()}`}>
       <div className="wiki__category-titleItem">
         <img src={wikiParchment} alt="" className="wikiParchment wiki__category-titleImage"/>
         <h2 className="wiki__category-title">{title}</h2>
