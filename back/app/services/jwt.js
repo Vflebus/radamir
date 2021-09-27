d4b786a63764dfc858ba10b15079f01d6add8363
Boris <vavallantin@hotmail.fr>
2021-09-27 15:19:14 +0200

    relecture jwt

--- back/app/services/jwt.js
+++ back/app/services/jwt.js
@@ -1,59 +1,63 @@
-const JWT = require('jsonwebtoken')
+const JWT = require("jsonwebtoken");
 
 module.exports = {
-    generateToken: user => {
-        try {
-            return JWT.sign(
-                {
-                    userId 
-                },
-                process.env.ACCESS_SECRET_TOKEN,
-                {
-                    expiresIn: '1m',
-                    algorithm: 'HS256'
-                }
-            );
-        } catch (error) {
-            console.log(error);
-            throw error;
+  generateToken: (user) => {
+    try {
+      return JWT.sign(
+        {
+          userId,
+        },
+        process.env.ACCESS_SECRET_TOKEN,
+        {
+          algorithm: "HS256",
+          expiresIn: "1m",
         }
-    },
-
-    validateToken: token => {
-        try {
-            JWT.verify(token, process.env.ACCESS_SECRET_TOKEN, {algorithms: ['HS256']});
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-    },
-
-    refreshToken: token => {
-        try {
-            return JWT.sign(
-                {
-                    userId
-                },
-                process.env.REFRESH_SECRET_TOKEN,
-                {
-                    expiresIn: '3m',
-                    algorithm: 'HS256'
-                }
-            );
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
-    },
-
-    validateRefreshToken: token => {
-        try {
-            JWT.verify(token, process.env.REFRESH_SECRET_TOKEN, {algorithms: ['HS256']});
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
+      );
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
-}
+  },
+
+  validateToken: (token) => {
+    try {
+      JWT.verify(token, process.env.ACCESS_SECRET_TOKEN, {
+        algorithms: ["HS256"],
+      });
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+
+  refreshToken: (token) => {
+    try {
+      return JWT.sign(
+        {
+          userId,
+        },
+        process.env.REFRESH_SECRET_TOKEN,
+        {
+          algorithm: "HS256",
+          expiresIn: "3m",
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+
+  validateRefreshToken: (token) => {
+    try {
+      JWT.verify(token, process.env.REFRESH_SECRET_TOKEN, {
+        algorithms: ["HS256"],
+      });
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+};
