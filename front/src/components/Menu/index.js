c2a2eae601946ca8c406b6b282aa9217d537acc2
Michael-M <mt.morales@hotmail.fr>
2021-09-27 10:28:09 +0200

    added useState to handle click on menu

--- front/src/components/Menu/index.js
+++ front/src/components/Menu/index.js
@@ -1,36 +1,55 @@
-import './style.scss';
+import { useState } from "react";
 
-import menuPlie from './images/menuPlie.png';
-import menuDeplie from './images/menuDeplie.png';
-import { NavLink } from 'react-router-dom';
+import "./style.scss";
 
+import menuPlie from "./images/menuPlie.png";
+import menuDeplie from "./images/menuDeplie.png";
+import { NavLink } from "react-router-dom";
 
+const Menu = ({ classes }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const Menu = ( {classes} ) => {
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const toggleMenu = () => {
-        let menuDeplie = document.getElementById('menuDeplie');
-        let menuPlie = document.getElementById('menuPlie');
-        menuPlie.classList.toggle('noDisplay');
-        menuDeplie.classList.toggle('inactive');
-    }
+  return (
+    <div id="menu">
+      <div
+        className={`menu ${classes} ${isOpen ? "" : "inactive"}`}
+      >
+        <img src={menuDeplie} alt="menu" className="menuImg" />
+        <div className="navLinks">
+          <NavLink exact to="/" className="links" activeClassName="selected">
+            Accueil
+          </NavLink>
+          <NavLink to="/wiki" className="links" activeClassName="selected">
+            Lien 2
+          </NavLink>
+          <NavLink to="/3" className="links" activeClassName="selected">
+            Lien 3
+          </NavLink>
+          <NavLink to="/4" className="links" activeClassName="selected">
+            Lien 4
+          </NavLink>
+        </div>
+      </div>
+      <div
+        className={`menu ${classes} ${isOpen ? "noDisplay" : ""}`}
+      >
+        <img
+          src={menuPlie}
+          alt="menu"
+          className="menuImg"
+          onClick={toggleMenu}
+        />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div id="menu">
-            <div className={"menu inactive "+classes} id="menuDeplie">
-                <img src={menuDeplie} alt="menu" className="menuImg" />
-                <div className="navLinks">
-                    <NavLink exact to="/" className="links" activeClassName="selected">Accueil</NavLink>
-                    <NavLink to="/wiki" className="links" activeClassName="selected">Lien 2</NavLink>
-                    <NavLink to="/3" className="links" activeClassName="selected">Lien 3</NavLink>
-                    <NavLink to="/4" className="links" activeClassName="selected">Lien 4</NavLink>
-                </div>
-            </div>
-            <div className={"menu "+classes} id="menuPlie">
-               <img src={menuPlie} alt="menu" className="menuImg" onClick={toggleMenu} />
-            </div>
-        </div>    
-    )
+Menu.defaultProps = {
+  classes: "",
 };
 
 export default Menu;
