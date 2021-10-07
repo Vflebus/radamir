b035ec43eeeb8d96b745bc8112103e215b450e6a
Vincent Flebus <vflebus@gmail.com>
2021-10-07 12:35:37 +0200

    gitflow-feature-stash: responsive_campaigns_page
    campaigns page is now responsive

--- front/src/components/CampaignList/index.js
+++ front/src/components/CampaignList/index.js
@@ -6,11 +6,27 @@ import bgShip from "../../assets/images/bgShip.png"
 const CampaignList = () => {
     return (
         <div className="campaignList">
+            <h1>CAMPAGNES</h1>
             <section className="sectionLeft">
-                <h1>CAMPAGNES</h1>
                 <img src={bgShip} alt="" className="shipImg"/>
             </section>
             <section className="sectionRight">
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
+                <CampaignCard />
                 <CampaignCard />
                 <CampaignCard />
                 <CampaignCard />
