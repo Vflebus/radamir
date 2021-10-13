800cf897715d42657f1497281483557385e47c71
Loïc B <loic.bonaventura@gmail.com>
2021-10-13 17:21:13 +0000

    Corrected update_campaign function in the function.sql migration

--- back/migrations/radamir_seeding.sql
+++ back/migrations/radamir_seeding.sql
@@ -1,7 +1,8 @@
 INSERT INTO "user"("username", "email", "password", "is_admin")
 VALUES
 ('NonoLeBoss', 'nono@leboss.pro', 'encrypted', true),
-('Boris_le_peon', 'boris@borisplanet.com', '12345', false);
+('Boris_le_peon', 'boris@borisplanet.com', '12345', false),
+('Lamba', 'lambda@basique.fr', '$2a$10$EZcdsDHt.MyKOOsKe0Ptt.JYrVru7a3QsDitu9/8//zy5ch7B.QxO', true);
 
 INSERT INTO "campaign"("campaign_name", "description", "created_at", "user_id")
 VALUES('La quête de Nono', 'Nono le Boss va se lancer dans un périple semé d''embûches afin de trouver comment sortir de VIM', NULL, 1);
@@ -12,8 +13,7 @@ VALUES (1, 1);
 INSERT INTO "note" ("title", "content", "is_private", "campaign_id", "user_id")
 VALUES
 ('La Croisade', 'On a croisé aleks', false, 1, 1),
-('La seconde croisade', 'On a croisé Guillaume Pet Hache de Paix', false, 1, 2)
-('Note publique d''utilisateur 3', 'son contenu', false, 1, 3)
-('Note privée d''utilisateur 3', 'son contenu', true, 1, 3)
-('Note publique d''utilisateur 1', 'son contenu', false, 1, 3)
-;
+('La seconde croisade', 'On a croisé Guillaume Pet Hache de Paix', false, 1, 2),
+('Note publique d''utilisateur 3', 'son contenu', false, 1, 3),
+('Note privée d''utilisateur 3', 'son contenu', true, 1, 3),
+('Note publique d''utilisateur 1', 'son contenu', false, 1, 3);
