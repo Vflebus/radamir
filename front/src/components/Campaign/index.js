ffd8619452690814336852a637cd80a79332deed
Vincent Flebus <vflebus@gmail.com>
2021-10-17 11:57:15 +0200

    Websocket for discord bot

--- front/src/components/Campaign/index.js
+++ front/src/components/Campaign/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
+import socketIOClient from "socket.io-client";
 
 import MotionWrapper from "../MotionWrapper";
 import ConfirmDelete from "../ConfirmDelete";
@@ -21,6 +22,7 @@ import "./campaign.scss";
 import Note from "./Note";
 
 const Campaign = () => {
+    const ENDPOINT = "http://164.90.223.174:4001/";
     const dispatch = useDispatch();
     const history = useHistory();
     const [deleteOpen, setDeleteOpen] = useState(false);
@@ -32,6 +34,15 @@ const Campaign = () => {
     const campaignLoader = useSelector(({ campaigns }) => campaigns.loading);
 
     const currentCampaign = list.find(campaign => campaign.id === +id);
+
+    const [imgUrl, setImgUrl] = useState("https://cdn.discordapp.com/attachments/837830452042661899/897226129709096960/Cennetig_le_Minutieux.jpg");
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT);
+        socket.on("ImgUrl", data => {
+          setImgUrl(data);
+          console.log(data)
+        }, [])
+    })
 
     const handleDelete = () => {
         dispatch(deleteCampaign(userId, +id));
@@ -74,10 +85,6 @@ const Campaign = () => {
     }
     
     const is_author = (userId === currentCampaign.user_id);
-
-    if (loading) {
-        return <Loading />;
-    }
     
     return (
         <MotionWrapper>
@@ -122,6 +129,9 @@ const Campaign = () => {
                         }
                     </section>
                 </section>
+                {loading && 
+                <div>Chargement...</div>}
+                {!loading &&
                 <section className="pageTwo">
                     <img src={bg2} alt="" className="bg2"/>
                     <h2>Notes</h2>
@@ -137,12 +147,13 @@ const Campaign = () => {
                         </section>
                         <section className="imageDiscord">
                             <h3>Illustration actuelle</h3>
-                            <img src="https://cdn.discordapp.com/attachments/837830452042661899/897226129709096960/Cennetig_le_Minutieux.jpg" alt="" className="discordImg"/>
+                            <img src={imgUrl} alt="" className="discordImg"/>
                         </section>
                     </section>
                     <button className="addNote" onClick={openAddModal}>Ajouter une nouvelle note</button>
                     <AddNoteModal open={isAddModalOpen} onClose={onAddModalClose} campaign_id={id}/>
                 </section>
+                }
             </div>
         </MotionWrapper>
     );
