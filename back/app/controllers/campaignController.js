1493bfa035a39da098c24cc0055bd4ec733f2f4d
Vincent Flebus <vflebus@gmail.com>
2021-11-15 15:45:11 +0100

    fix

--- back/app/controllers/campaignController.js
+++ back/app/controllers/campaignController.js
@@ -4,8 +4,8 @@ const campaignController = {
     
     getAllCampaigns: async (request, response) => {
         try {
-            console.log(`request params: ${request.data}`);
-            const campaigns = await Campaign.getAllCampaigns(request.data.user_id);
+            console.log(`request params: ${request.data.args.user_id}`);
+            const campaigns = await Campaign.getAllCampaigns(request.data.args.user_id);
 			response.json(campaigns);
         } catch (error) {
 			console.error(error);
