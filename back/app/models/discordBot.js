909040eff1825e8e85b9dcadf421662c16209ae2
Vincent Flebus <vflebus@gmail.com>
2022-01-12 16:43:32 +0100

    discord bot cleaned up !

--- back/app/models/discordBot.js
+++ back/app/models/discordBot.js
@@ -2,38 +2,18 @@ const io = require('../../index.js');
 
 const discordBot = (message) => {
   if (message.content.startsWith('http') && (message.content.endsWith('jpeg') || message.content.endsWith('jpg') || message.content.endsWith('gif') || message.content.endsWith('png'))) {
+    // Le message commence par http et finit par une extension d'image, c'est très probablement un lien d'image ! On récupère le contenu du message dans message.content
     console.log(`Il correspond ! ${message.content}`);
     io.emit("ImgUrl", message.content);
   }
-  else if (message.embeds[0]) {
-    console.log('message.embeds[0] found');
-    if (message.author.bot) return;
-      
-    //Array of the arguments after the command in the message
-    const args = message.content.split(/ +/);
-    console.log(message.content);
-    
-    console.log(`args[0]: ${args[0]}`);
-
-    io.emit("ImgUrl", `${args[0]}`);
-  }
-  else if (message.attachments.first([1]).length > 0){
-    console.log(`message.attachments.first([1])[0].url: ${message.attachments.first([1])[0].url}`);
-    io.emit("ImgUrl", `${message.attachments.first([1])[0].url}`)
-    console.log(`message: ${message}`);
-    //! demandes au front de poster une nouvelle image in real time dans la page campagne
+  else if (message.attachments.first().url){
+    //attachments est une collection contenant les documents ajoutés au message. Une collection est une classe discord.js basée sur Map. Sa méthode first() renvoie le premier élément de la collection : un object avec une prop url
+    console.log(`found message.attachments.first().url: ${message.attachments.first().url}`);
+    io.emit("ImgUrl", `${message.attachments.first().url}`)
   } 
   else {
-    console.log('ça ne me concerne pas');
-    console.log(`ce qui ne me concerne pas : ${message.content}`);
-    console.log(`message: ${message}`);
+    console.log(`ça ne me concerne pas : ${message.content}`);
   }
 };
-
-//Ligne pour trouver l'url de l'image(file) postée sur le canal
-//console.log(message.attachments.first([1])[0].url);
-
-// ligne pour trouver l'url de l'image(URL) postée sur le discord
-//console.log(message.embeds[0].url);
 
 module.exports = discordBot;
