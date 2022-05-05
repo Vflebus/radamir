d33ceb94f59480961b9909b6a20ee3588c576b3b
Vincent Flebus <vflebus@gmail.com>
2022-05-05 13:25:05 +0200

    socket error connection

--- front/src/components/Campaign/index.js
+++ front/src/components/Campaign/index.js
@@ -12,7 +12,13 @@ import AddNoteModal from "../AddNoteModal";
 import { deleteCampaign, setCampaignInput } from "../../actions/campaigns";
 import { clearError } from "../../actions/error";
 
-import { setTitle, setType, setContent, setImageUrl, fetchNotes } from "../../actions/notes";
+import {
+  setTitle,
+  setType,
+  setContent,
+  setImageUrl,
+  fetchNotes,
+} from "../../actions/notes";
 
 import carte from "../../assets/images/CarteRadamir.png";
 import bg2 from "../../assets/images/bg2.png";
@@ -22,205 +28,209 @@ import "./campaign.scss";
 import Note from "./Note";
 
 const Campaign = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const [deleteOpen, setDeleteOpen] = useState(false);
-    const [editOpen, setEditOpen] = useState(false);
-    const { id } = useParams();
-    const userId = useSelector(({ user: { loggedUser } }) => loggedUser.id);
-    const { list } = useSelector(({ campaigns }) => campaigns);
-    const { loading } = useSelector(({ notes }) => notes);
-    const campaignLoader = useSelector(({ campaigns }) => campaigns.loading);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [editOpen, setEditOpen] = useState(false);
+  const { id } = useParams();
+  const userId = useSelector(({ user: { loggedUser } }) => loggedUser.id);
+  const { list } = useSelector(({ campaigns }) => campaigns);
+  const { loading } = useSelector(({ notes }) => notes);
+  const campaignLoader = useSelector(({ campaigns }) => campaigns.loading);
 
-    const currentCampaign = list.find(campaign => campaign.id === +id);
+  const currentCampaign = list.find((campaign) => campaign.id === +id);
 
-    const [imgUrl, setImgUrl] = useState("https://cdn.discordapp.com/attachments/837830452042661899/897226129709096960/Cennetig_le_Minutieux.jpg");
+  const [imgUrl, setImgUrl] = useState(
+    "https://cdn.discordapp.com/attachments/837830452042661899/897226129709096960/Cennetig_le_Minutieux.jpg"
+  );
 
-    
-    const ENDPOINT = process.env.SERVERIO;
-    useEffect(() => {
-        console.log('connecting socket');
-        const socket = socketIOClient(ENDPOINT, {
-            reconnection: false
-        });
-        socket.on("ImgUrl", data => {
-          setImgUrl(data);
-          console.log(data)
-        });
-        return function cleanup() {
-            console.log('disconnecting socket');
-            socket.disconnect();
-        }
-    }, [ENDPOINT]);
-
-    const handleDelete = () => {
-        dispatch(deleteCampaign(userId, +id));
-        history.push("/campagnes");
+  const ENDPOINT = process.env.SERVERIO;
+  useEffect(() => {
+    console.log("connecting socket");
+    const socket = socketIOClient(ENDPOINT, {
+      reconnection: false,
+    });
+    socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+    socket.on("ImgUrl", (data) => {
+      setImgUrl(data);
+      console.log(data);
+    });
+    return function cleanup() {
+      console.log("disconnecting socket");
+      socket.disconnect();
     };
+  }, [ENDPOINT]);
 
-    const handleOpenEdit = () => {
-        dispatch(setCampaignInput("campaign_name", currentCampaign.campaign_name));
-        dispatch(setCampaignInput("description", currentCampaign.description));
-        setEditOpen(true);
-    };
+  const handleDelete = () => {
+    dispatch(deleteCampaign(userId, +id));
+    history.push("/campagnes");
+  };
 
-    const handleCloseEdit = () => {
-        dispatch(clearError());
-        dispatch(setCampaignInput("campaign_name", ""));
-        dispatch(setCampaignInput("description", ""));
-        setEditOpen(false);
-    };
+  const handleOpenEdit = () => {
+    dispatch(setCampaignInput("campaign_name", currentCampaign.campaign_name));
+    dispatch(setCampaignInput("description", currentCampaign.description));
+    setEditOpen(true);
+  };
 
-    const addModalFromImage = () => {
-        dispatch(setImageUrl(imgUrl));
-        openAddModal();
-    }
+  const handleCloseEdit = () => {
+    dispatch(clearError());
+    dispatch(setCampaignInput("campaign_name", ""));
+    dispatch(setCampaignInput("description", ""));
+    setEditOpen(false);
+  };
 
-    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-    const openAddModal = () => {
-        setIsAddModalOpen(true);
-    };
-    const onAddModalClose = () => {
-        setIsAddModalOpen(false);
-        dispatch(setType("publique"));
-        dispatch(setTitle(""));
-        dispatch(setContent(""));
-        dispatch(clearError());
-    };
-    
-    useEffect (() => {
-        dispatch(fetchNotes(id, userId));
-    }, [dispatch, id, userId]);
+  const addModalFromImage = () => {
+    dispatch(setImageUrl(imgUrl));
+    openAddModal();
+  };
 
-    const notesList = useSelector(({ notes }) => notes.list);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const openAddModal = () => {
+    setIsAddModalOpen(true);
+  };
+  const onAddModalClose = () => {
+    setIsAddModalOpen(false);
+    dispatch(setType("publique"));
+    dispatch(setTitle(""));
+    dispatch(setContent(""));
+    dispatch(clearError());
+  };
 
-    if (campaignLoader) {
-        return <Loading />;
-    }
-    
-    const is_author = (userId === currentCampaign.user_id);
-    
-    return (
-        <MotionWrapper>
-            <div className="campaign">
-                <h1>{currentCampaign.campaign_name}</h1>
-                <section className="pageOne">
-                    <section className="sectionCarte">
-                        <img src={carte} alt="" />
-                    </section>
-                    <section className="sectionResume">
-                        <h2>Résumé de la campagne :</h2>
-                        <p>{currentCampaign.description}</p>
-                        {
-                            is_author &&
-                            <>
-                                <button
-                                    type="button"
-                                    className="admin-button"
-                                    onClick={handleOpenEdit}
-                                >
-                                    Modifier Campagne
-                                </button>
-                                <EditCampaignModal
-                                    open={editOpen}
-                                    onClose={handleCloseEdit}
-                                    campaignId={+id}
-                                />
-                                <button
-                                    type="button"
-                                    className="admin-button delete-wiki"
-                                    onClick={() => setDeleteOpen(true)}
-                                >
-                                    Supprimer Campagne
-                                </button>
-                                <ConfirmDelete
-                                    open={deleteOpen}
-                                    title={currentCampaign.campaign_name}
-                                    onClose={() => setDeleteOpen(false)}
-                                    onDelete={handleDelete}
-                                />
-                            </>
-                        }
-                    </section>
-                </section>
-                {loading && 
-                <div>Chargement...</div>}
-                {!loading &&
-                <section className="pageTwo">
-                    <img src={bg2} alt="" className="bg2"/>
-                    <h2>Notes</h2>
-                    <section className="allNotes">
+  useEffect(() => {
+    dispatch(fetchNotes(id, userId));
+  }, [dispatch, id, userId]);
 
-                        <section className="notesPrivees">
+  const notesList = useSelector(({ notes }) => notes.list);
 
-                            <h3>Mes notes privées</h3>
+  if (campaignLoader) {
+    return <Loading />;
+  }
 
-                            {notesList.myPrivates.map(
-                                (note) => 
-                                    <Note
-                                        title={note.title}
-                                        content={note.content}
-                                        note_id={note.id}
-                                        creator_id={note.user_id}
-                                        campaign_id={id}
-                                        user_id={userId}
-                                        is_private={note.is_private}
-                                        image_url={note.image_url}
-                                        key={note.id}
-                                    />
-                                )
-                            }
-                        </section>
+  const is_author = userId === currentCampaign.user_id;
 
-                        <section className="notesPubliques">
-                            <h3>Notes du groupe</h3>
-                            {notesList.myPublics.map(
-                                (note) => 
-                                    <Note 
-                                        title={note.title}
-                                        content={note.content}
-                                        note_id={note.id}
-                                        creator_id={note.user_id}
-                                        campaign_id={id}
-                                        user_id={userId}
-                                        is_private={note.is_private}
-                                        image_url={note.image_url}
-                                        key={note.id}
-                                    />
-                                )
-                            }
-                            {notesList.publics.map(
-                                (note) => 
-                                    <Note
-                                        title={note.title}
-                                        content={note.content}
-                                        note_id={note.id}
-                                        creator_id={note.user_id}
-                                        campaign_id={id}
-                                        user_id={userId}
-                                        is_private={note.is_private}
-                                        image_url={note.image_url}
-                                        key={note.id}
-                                    />
-                                )
-                            }
-                        </section>
+  return (
+    <MotionWrapper>
+      <div className="campaign">
+        <h1>{currentCampaign.campaign_name}</h1>
+        <section className="pageOne">
+          <section className="sectionCarte">
+            <img src={carte} alt="" />
+          </section>
+          <section className="sectionResume">
+            <h2>Résumé de la campagne :</h2>
+            <p>{currentCampaign.description}</p>
+            {is_author && (
+              <>
+                <button
+                  type="button"
+                  className="admin-button"
+                  onClick={handleOpenEdit}
+                >
+                  Modifier Campagne
+                </button>
+                <EditCampaignModal
+                  open={editOpen}
+                  onClose={handleCloseEdit}
+                  campaignId={+id}
+                />
+                <button
+                  type="button"
+                  className="admin-button delete-wiki"
+                  onClick={() => setDeleteOpen(true)}
+                >
+                  Supprimer Campagne
+                </button>
+                <ConfirmDelete
+                  open={deleteOpen}
+                  title={currentCampaign.campaign_name}
+                  onClose={() => setDeleteOpen(false)}
+                  onDelete={handleDelete}
+                />
+              </>
+            )}
+          </section>
+        </section>
+        {loading && <div>Chargement...</div>}
+        {!loading && (
+          <section className="pageTwo">
+            <img src={bg2} alt="" className="bg2" />
+            <h2>Notes</h2>
+            <section className="allNotes">
+              <section className="notesPrivees">
+                <h3>Mes notes privées</h3>
 
-                        <section className="imageDiscord">
-                            <h3>Illustration</h3>
-                            <div className="imgContainer">
-                                <img src={imgUrl} alt="" className="discordImg" onClick={addModalFromImage}/>
-                            </div>
-                        </section>
+                {notesList.myPrivates.map((note) => (
+                  <Note
+                    title={note.title}
+                    content={note.content}
+                    note_id={note.id}
+                    creator_id={note.user_id}
+                    campaign_id={id}
+                    user_id={userId}
+                    is_private={note.is_private}
+                    image_url={note.image_url}
+                    key={note.id}
+                  />
+                ))}
+              </section>
 
-                    </section>
-                    <button className="addNote" onClick={openAddModal}>Ajouter une nouvelle note</button>
-                    <AddNoteModal open={isAddModalOpen} onClose={onAddModalClose} campaign_id={id}/>
-                </section>
-                }
-            </div>
-        </MotionWrapper>
-    );
+              <section className="notesPubliques">
+                <h3>Notes du groupe</h3>
+                {notesList.myPublics.map((note) => (
+                  <Note
+                    title={note.title}
+                    content={note.content}
+                    note_id={note.id}
+                    creator_id={note.user_id}
+                    campaign_id={id}
+                    user_id={userId}
+                    is_private={note.is_private}
+                    image_url={note.image_url}
+                    key={note.id}
+                  />
+                ))}
+                {notesList.publics.map((note) => (
+                  <Note
+                    title={note.title}
+                    content={note.content}
+                    note_id={note.id}
+                    creator_id={note.user_id}
+                    campaign_id={id}
+                    user_id={userId}
+                    is_private={note.is_private}
+                    image_url={note.image_url}
+                    key={note.id}
+                  />
+                ))}
+              </section>
+
+              <section className="imageDiscord">
+                <h3>Illustration</h3>
+                <div className="imgContainer">
+                  <img
+                    src={imgUrl}
+                    alt=""
+                    className="discordImg"
+                    onClick={addModalFromImage}
+                  />
+                </div>
+              </section>
+            </section>
+            <button className="addNote" onClick={openAddModal}>
+              Ajouter une nouvelle note
+            </button>
+            <AddNoteModal
+              open={isAddModalOpen}
+              onClose={onAddModalClose}
+              campaign_id={id}
+            />
+          </section>
+        )}
+      </div>
+    </MotionWrapper>
+  );
 };
 
 export default Campaign;
